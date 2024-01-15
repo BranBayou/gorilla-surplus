2353008ee123d93683584d0356e7f341300adc87
BranBayou <berhan.baye@gmail.com>
2024-01-15 17:54:21 +0300

    Sub popup working

--- assets/js/home/popup.js
+++ assets/js/home/popup.js
@@ -1,6 +1,8 @@
+/* Catalog popup */
+
 const catalogBtn = document.querySelector('.catalog');
 const popupScreen = document.querySelector('.popup');
-const overlay = document.querySelector('.overlay'); // Add this line
+const overlay = document.querySelector('.overlay'); 
 
 let isMouseOverButton = false;
 let isMouseOverPopup = false;
@@ -28,13 +30,55 @@ popupScreen.addEventListener('mouseout', () => {
 function showPopup() {
   if (isMouseOverButton || isMouseOverPopup) {
     popupScreen.classList.remove('d-none');
-    overlay.classList.remove('d-none'); // Show the overlay
+    overlay.classList.remove('d-none');
   }
 }
 
 function hidePopup() {
   if (!isMouseOverButton && !isMouseOverPopup) {
     popupScreen.classList.add('d-none');
-    overlay.classList.add('d-none'); // Hide the overlay
+    overlay.classList.add('d-none');
+  }
+}
+
+/* Catalog sub popup for every button hover */
+
+const gunsRiflesBtn = document.getElementById('guns-and-rifles-btn');
+
+const gunsRiflesList = document.getElementById('guns-and-rifles');
+
+let isMouseOverSubButton = false;
+let isMouseOverSubPopup = false;
+
+gunsRiflesBtn.addEventListener('mouseover', () => {
+  isMouseOverSubButton = true;
+  showGunsPopup();
+});
+
+gunsRiflesBtn.addEventListener('mouseout', () => {
+  isMouseOverSubButton = false;
+  hideGunsPopup();
+});
+
+gunsRiflesList.addEventListener('mouseenter', () => {
+  isMouseOverSubPopup = true;
+  showGunsPopup();
+});
+
+gunsRiflesList.addEventListener('mouseleave', () => {
+  isMouseOverSubPopup = false;
+  hideGunsPopup();
+});
+
+function showGunsPopup() {
+  if (isMouseOverSubButton || isMouseOverSubPopup) {
+    gunsRiflesList.classList.remove('d-none');
+    overlay.classList.remove('d-none');
+  }
+}
+function hideGunsPopup() {
+  if (!isMouseOverSubButton && !isMouseOverSubPopup) {
+    gunsRiflesList.classList.add('d-none');
+    overlay.classList.add('d-none');
   }
 }
